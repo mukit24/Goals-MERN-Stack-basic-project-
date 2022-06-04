ed93b489c91a37fc91a047ce3cd9b9e3f86c7f0b
mukit24 <mukithasanpranto4@gmail.com>
2022-06-04 11:00:48 +0600

    mongoose_api_op

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,25 +1,83 @@
+const Goal = require('../models/goalModel')
+
 // @desc get goals
 // route GET /api/goals
-const getGoals = (req,res) => {
-    res.status(200).json({ message:  'get goal' })
+const getGoals = async (req,res) => {
+    try {
+        const goals = await Goal.find({})
+        res.status(210).json({
+            status: 'Success',
+            data: {
+                goals
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed',
+            message: error
+        })
+    }
 }
 
 // @desc set goals
 // route POST /api/goals
-const setGoal = (req,res) => {
-    res.status(200).json({ message:  'set goal' })
+const setGoal = async (req,res) => {
+    const goal = new Goal(req.body)
+    try {
+        await goal.save()
+        res.status(210).json({
+            status: 'Success',
+            data: {
+                goal
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed',
+            message: error
+        })
+    }
 }
 
 // @desc update goals
 // route PUT /api/goals
-const updateGoal = (req,res) => {
-    res.status(200).json({ message:  `update goal ${req.params.id}` })
+const updateGoal = async (req,res) => {
+    try {
+        const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
+            new : true,
+            runValidators : true
+        })
+        res.status(210).json({
+            status: 'Success',
+            data: {
+                updatedGoal
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed',
+            message: error
+        })
+    }
 }
 
 // @desc delete goals
 // route DELETE /api/goals
-const deleteGoal = (req,res) => {
-    res.status(200).json({ message : `delete goal ${req.params.id}`})
+const deleteGoal = async (req,res) => {
+    try {
+        await Goal.findByIdAndDelete(req.params.id)
+        res.status(210).json({
+            status: 'Success',
+            data: {
+                
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed',
+            message: error
+        })
+    }
 }
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-const app = express()
+connectDB()
 
+const app = express()
+app.use(express.json())
 app.use('/api/goals',require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
